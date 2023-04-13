4167bff123e60aa63ba61f6b4df7d65632000151
Augustine Cudjoe <auguscudjoe94@gmail.com>
2023-04-14 00:14:36 +0200

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <h2>welcome home</h2> 
+      <Navbar/> 
+     
     </div>
   );
 }
